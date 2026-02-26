6b02aa406459dc356dcd457df3ba301e84284d2f
Stolpqvist <stolpqvist@gmail.com>
2026-02-26 19:38:20 +0100

    Documentation and import changes

--- preprocessing/pre_roberta.py
+++ preprocessing/pre_roberta.py
@@ -3,13 +3,23 @@ This module is the main module that handles the preprocessing required for the R
 It takes a filename when creating the class, then handles id creation and preprocessing.
 It outputs a stacked tensor.
 
+Class:
+    DataProcessor: handles the data preprocessing of the roberta model.
+
+Attributes:
+    file(str): the filename/path to dataset.
+    label2id(defaultdict(int)): the label to id assigner that defines the id as the length of the dictionary.
+    tokenizer (AutoTokenizer) = the tokenizer chosen for the XLMRobertaForSequenceClassification model.
+
+Returns:
+    row_yielder = yields a row in a dataframe.
+    preprocessing = returns a list of tokenized torch.Tensors.
 
 """
 import pandas as pd
 import torch
 from transformers import AutoTokenizer
 from collections import defaultdict
-import sys
 
 
 
@@ -36,25 +46,46 @@ class DataProcessor:
         self.tokenizer = AutoTokenizer.from_pretrained('xlm-roberta-base')
 
     def row_yielder(self):
+        """
+        Reads the class instance of self.file, opens the csv file and yields the file
+        row by row.
 
+        Return/Yields:
+            row, list[str] = the data of the dataframe from a particular row
+        """
         df = pd.read_csv(self.file)
         for _, row in df.iterrows():
             yield row
     
     def preprocessing(self):
+       """
+        Handles the logic of preprocessing the data from the provided dataframe.
+        It calls row yielder and then processes it with 2 inner functions.
         
-        #TODO Do we want to yield it row by row, and that each item in the list is
-        #a row? Or do we want to simply process it row by row and train it as such
-        #TODO Figure out training sequence
+        Inner functions:
+            label_extractor(str): extract and handles the label logic of assigning each label a value
+                                  and each value a label for label identification.
+
+            auto_tok(str): tokenizes a given column and returns it as a tensor.
+
+        Returns:
+            list_tok(list(torch.Tensor)): all the extracted and tokenized columns as a list.
+       """ 
         for row in self.row_yielder():
 
             def label_extractor(label: str) -> None:
+                """
+                Extracts the labels and enters it into a defauldict to handle label -> id and
+                id -> label assignment.
+                """
 
-                label_id = self.label2id[label]
+                label_id = self.label2id[label] 
                 self.id2label[label_id] = label
             
             def auto_tok(column: str) -> torch.Tensor: 
-                #print(f"The text column is: {column}")
+                """
+                Takes a given column as a string and returns a tokenized torch.Tensor
+                """
                 return self.tokenizer(column, return_tensors="pt")
 
            # def stacker(list_pt:list[torch.Tensor]) -> torch.Tensor:
@@ -77,6 +108,7 @@ class DataProcessor:
 
 
 if __name__ == "__main__":
+    import sys
     file = sys.argv[1]
     dp = DataProcessor(file)
     dp.preprocessing()
