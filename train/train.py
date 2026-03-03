a2bc3b19dbd6f0937a817c53f07247680f99d2c6
madame-croissant <adele.adele.me@gmail.com>
2026-03-03 13:03:05 +0100

    plus fix

--- train/train.py
+++ train/train.py
@@ -1,24 +1,11 @@
 from preprocessing.pre_roberta import DataProcessor
 from model.roberta import CustomXLMRoberta
-from torch.utils.data import DataLoader, Dataset, TensorDataset
+from torch.utils.data import DataLoader, TensorDataset
 import torch
 import torch.nn as nn
 import numpy as np
 
-class FieldDataset(Dataset):
-    def __init__(self, fields, labels):
-        self.fields = fields
-        self.labels = labels
-    
-    def __len__(self):
-        return len(self.labels)
-    
-    def __getitem__(self, idx):
-    
-        return (
-            self.fields[idx],
-            self.labels[idx]
-        )
+
 
 class ModelTrain:
     def __init__(self, lr, n_epochs, batch_size, dropout):
@@ -28,49 +15,25 @@ class ModelTrain:
         self.batch_size = batch_size
         self.criterion = nn.CrossEntropyLoss()
         self.dropout = dropout
-    
-    #How to handle DataProcessor??
-    #train_pr = DataProcessor(train_fold)
-    #train_pr.label_extractor(label_cl)
-
-    def evaluate(self, model, val_data, label_cl):
-        model.eval()
-        losses = []
-        all_pred = []
-        true_labels = []
-        
-        val_pr = DataProcessor(val_data)
-        val_pr.label_extractor(label_cl)
-
-        val_data = val_pr.preprocessing()
-
-        val_tensor = TensorDataset(val_data)
-
-        #with torch.no_grad():
-           # for fields, b_labels in da
 
 
     def training_loop(self, data, label_cl):
 
         #extr num_labels
-        train_labels = data[label_cl] #NOT tensors
-        num_classes = len(np.unique(train_labels))
+        #train_labels = data[label_cl] #NOT tensors
+        #num_classes = len(np.unique(train_labels))
 
         #Prepare the data:
-        train_pr = DataProcessor(data)
-        train_pr.label_extractor(label_cl)
+        train_fold = DataProcessor(data)
+        train_fold.label_extractor()
+        train_dataloader = DataLoader(train_fold, batch_size=self.batch_size, shuffle=False)
 
-
-        train_data = train_pr.preprocessing() #Tensors # list of 5 dicts, each (N, 512)
-
-        #Labels to tensors
-
-        label_tensor = torch.tensor(list(train_pr.label2id[label] for label in train_labels), dtype=torch.long)
+        print("Data loaded successfully")
 
         #Init the model
 
         model = CustomXLMRoberta(
-                    num_classes=num_classes,
+                    num_classes=len(train_fold.label2id.keys()),
                     hidden_dropout=self.dropout
                 )   
         
@@ -80,8 +43,6 @@ class ModelTrain:
 
         
         #Building datasets
-        train_dataset = FieldDataset(train_data, label_tensor)
-        train_dataloader = DataLoader(train_dataset, batch_size=self.batch_size, shuffle=False)
 
         total_losses = 0
         all_preds = []
@@ -108,7 +69,7 @@ class ModelTrain:
                 all_preds.extend(preds.tolist())
                 all_labels.extend(b_labels.tolist())
             
-            self.evaluate(model, val_data, label_cl)
+            #self.evaluate(model, val_data, label_cl)
             #accuracy per epoch
             correct = sum(pred == label for pred, label in zip(all_preds, all_labels))
             accuracy = correct / len(all_labels)
@@ -117,7 +78,21 @@ class ModelTrain:
         
         #return model
 
-        #EVALUATION
+    def evaluate(self, model, val_data, label_cl):
+        model.eval()
+        losses = []
+        all_pred = []
+        true_labels = []
+        
+        val_pr = DataProcessor(val_data)
+        val_pr.label_extractor(label_cl)
+
+        val_data = val_pr.preprocessing()
+
+        val_tensor = TensorDataset(val_data)
+
+        #with torch.no_grad():
+           # for fields, b_labels in d
 
 
 
