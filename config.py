18417a6e335d0625f342ca204d49a3a1ef4716ed
Stolpqvist <stolpqvist@gmail.com>
2026-03-17 15:41:39 +0100

    All so far joint, added dynamic model checker

--- config.py
+++ config.py
@@ -1,4 +1,11 @@
 from dataclasses import dataclass
+from enum import Enum
+
+class Model(Enum):
+    roberta = "roberta"
+    rnn = "rnn"
+    cnn = "cnn"
+
 
 
 @dataclass
@@ -10,4 +17,9 @@ class Config:
     lr:         float,
     dropout:    float,
     columns:    InitVar[list | str]
+
+    def __post_init__(self):
+        if not isinstance(self.model, Model):
+            raise ValueError(f"Invalid model. Choose from: {[m.value for m in Model]}")
+
     
